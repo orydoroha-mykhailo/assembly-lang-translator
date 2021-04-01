a5e1939c68a1eca9b0373652619d7ab68ca4c7f5
Mykhailo Orydoroha <orydoroha.mykhailo@gmail.com>
2021-04-01 18:03:28 +0300

    add ToUpper() func

--- translator.cpp
+++ translator.cpp
@@ -1,1 +1,11 @@
 #include "translator.h"
+
+using namespace std;
+
+#include <string>
+
+string ToUpper(string str) {
+  for (size_t i = 0; i < str.size(); i++) 
+    str[i] = toupper(str[i]);
+  return str;
+}
