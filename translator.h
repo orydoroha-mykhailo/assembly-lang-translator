ae49fb4381bc8acb9862250f789b723f012763c4
Mykhailo Orydoroha <orydoroha.mykhailo@gmail.com>
2021-04-01 17:52:56 +0300

    fix some issues

--- translator.h
+++ translator.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "analysis.h"
+
+#include <iostream>
+#include <fstream>
+
+class Translator {
+public:
+  Translator(const std::string& file_name);
+
+  void createListing();
+
+  void outAllLexems();
+  
+private:
+  std::string file_name_;
+
+  std::vector<Lexems> all_lexems_;
+};
